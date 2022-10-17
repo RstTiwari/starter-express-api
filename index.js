d8ad47caffebcd62f2a23c8103b1b1087c3309dc
RstTiwari <iamrrt2121@gmail.com>
2022-10-17 09:50:15 +0530

    added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const  mongoose = require("mongoose")
 const app = express()
+let port = 3000
 app.all('/signin', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
@@ -12,4 +14,15 @@ app.all("/signup", (req, res) => {
   }
   res.send(response);
 });
-app.listen(process.env.PORT || 3000)
+
+const CONNECTION_URL =
+  "mongodb://myfac8ry:myfac8ry@ac-z992smf-shard-00-00.fmmrhks.mongodb.net:27017,ac-z992smf-shard-00-01.fmmrhks.mongodb.net:27017,ac-z992smf-shard-00-02.fmmrhks.mongodb.net:27017/?ssl=true&replicaSet=atlas-piyukq-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(port, () =>
+      console.log(`Server Running on Port: http://localhost:${port}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
