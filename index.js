f3eb6a997f9844744fb6b2912c2f7c7ea0438718
RstTiwari <iamrrt2121@gmail.com>
2022-10-17 09:42:04 +0530

    added

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ app.all('/signin', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+app.all("/signup", (req, res) => {
+  console.log("Just got a request!");
+  res.send("Yo buddy!");
+});
 app.listen(process.env.PORT || 3000)
